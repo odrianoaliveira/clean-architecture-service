dc5838fb9c77a7f84b8c1645a0f99ab1e934bcc1
Adriano Oliveira <adriano.silvadeoliveira@signavio.com>
2020-04-21 11:16:53 +0200

    WIP: implement the spring Application

--- application/spring/src/main/java/tech/adriano/service/app/spring/Application.java
+++ application/spring/src/main/java/tech/adriano/service/app/spring/Application.java
@@ -1,8 +1,12 @@
 package tech.adriano.service.app.spring;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) {
-	
+		SpringApplication.run(Application.class, args);
 	}
 }
